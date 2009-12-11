115b1e9aff7565771e20f15d89769aa61fbd468d
Luis Perichon <luigi@luigi-laptop.(none)>
2009-12-11 17:51:24 -0300

    nice error message on activations when user already active

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -2,23 +2,32 @@ class ActivationsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
 
   def new
-    @user = User.find_using_perishable_token(params[:activation_code], 1.week) || (raise Exception)
-    raise Exception if @user.active?
+    @user = User.find_using_perishable_token(params[:activation_code], 1.week)
+    unless @user
+      flash[:notice] = t('activations.perishable_token_not_found')
+      redirect_to login_url
+      return
+    end
+    if @user.active?
+      flash[:notice] = t('activations.already_active')
+      redirect_to login_url
+    end
   end
 
   def create
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
 
-      raise Exception if @user.active?
-
-      if @user.activate!(params)
-        @user.deliver_activation_confirmation!
-        flash[:notice] = t('activations.create.success')
-        redirect_to dashboard_path
-      else
-        render :action => :new
-      end
+    if @user.active?
+      flash[:notice] = t('activations.already_active')
+      redirect_to login_url
     end
-  
 
+    if @user.activate!(params)
+      @user.deliver_activation_confirmation!
+      flash[:notice] = t('activations.create.success')
+      redirect_to dashboard_path
+    else
+      render :action => :new
+    end
+  end
 end
