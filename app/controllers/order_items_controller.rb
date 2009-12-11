ab01719c9508e81d53e0087855b5285925071ca7
Luis Perichon <luigi@luigi-laptop.(none)>
2009-12-11 16:56:02 -0300

    validate new order items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,10 +2,15 @@ class OrderItemsController < UserApplicationController
   def create
     @order = current_restaurant.orders.find(params[:order_id])
     @order_item = @order.order_items.new(params[:order_item])
-    @order_item.save
 
-    respond_to do |format|
-      format.js {}
+    if @order_item.save
+      respond_to do |format|
+        format.js {}
+      end
+    else
+      render(:update) do |page|
+        page.replace("#new_order_item", :partial => 'form', :locals => {:order_item => @order_item})
+      end
     end
   end
 end
