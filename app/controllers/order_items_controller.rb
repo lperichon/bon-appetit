e88c02f7e9c9943d81a13ecde544b32c81de4bf1
Luis Perichon <luigi@luigi-laptop.(none)>
2009-12-15 10:39:41 -0300

    update order items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -13,4 +13,15 @@ class OrderItemsController < UserApplicationController
       end
     end
   end
+
+  def update
+    @order = current_restaurant.orders.find(params[:order_id])
+    @order_item = @order.order_items.find(params[:id])
+
+    @order_item.update_attributes(params[:order_item])
+
+    respond_to do |format|
+      format.js {}
+    end
+  end
 end
