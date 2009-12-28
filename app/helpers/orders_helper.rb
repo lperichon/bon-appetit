c76c0ff5ce4eb366e2629107834f0e24ded4d796
Luis Perichon <luigi@luigi-laptop.(none)>
2009-12-28 17:35:43 -0300

    i18n on orders helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,5 +1,5 @@
 module OrdersHelper
   def order_status_label(order)
-    order.closed? ? 'closed' : 'open'
+    order.closed? ? Order.human_attribute_name('closed') : Order.human_attribute_name('open')
   end
 end
