a0df67fbcba76d2dd283c3592685dd8f7b92bae5
Luis Perichon <luigi@luigi-laptop.(none)>
2010-01-04 12:54:04 -0300

    unused method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user_session, :current_user, :current_restaurant
 
   private
-    def login_required
-      true
-    end
-
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
       @current_user_session = UserSession.find
