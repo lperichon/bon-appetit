b99f34983ee6988370513f86df36e8c78e8ee1da
Luis Perichon <luigi@luigi-laptop.(none)>
2010-01-07 18:42:34 -0300

    fix tests

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -3,7 +3,7 @@ require 'blueprints'
 
 class ProfilesControllerTest < ActionController::TestCase
   context 'when user is not logged in' do
-    setup { get :edit }
+    setup { get :show }
     should_redirect_to("the login page") { login_url }
     should_set_the_flash_to 'You must be logged in to access this page'
   end
@@ -11,17 +11,19 @@ class ProfilesControllerTest < ActionController::TestCase
   context 'logged in as a normal user' do
     setup { login }
 
-    context "get edit" do
-      setup { get :edit }
+    context "get show" do
+      setup { get :show }
       should_respond_with :success
     end
 
-    context "update product type" do
+    context "update profile" do
       setup do
         put :update, :user => {:first_name => 'Bob'}
       end
 
-      should_redirect_to("the dashboard") { dashboard_path }
+      should 'change the name' do
+        assert_equal 'Bob', assigns(:user).first_name
+      end
     end
   end
 end
--- test/functional/restaurants_controller_test.rb
+++ test/functional/restaurants_controller_test.rb
@@ -3,7 +3,7 @@ require 'blueprints'
 
 class RestaurantsControllerTest < ActionController::TestCase
   context 'when user is not logged in' do
-    setup { get :edit }
+    setup { get :show }
     should_redirect_to("the login page") { login_url }
     should_set_the_flash_to 'You must be logged in to access this page'
   end
@@ -11,8 +11,8 @@ class RestaurantsControllerTest < ActionController::TestCase
   context 'logged in as a normal user' do
     setup { login }
 
-    context "get edit" do
-      setup { get :edit }
+    context "get show" do
+      setup { get :show }
       should_respond_with :success
     end
 
@@ -21,7 +21,9 @@ class RestaurantsControllerTest < ActionController::TestCase
         put :update, :restaurant => {:name => 'New Name'}
       end
 
-      should_redirect_to("the dashboard") { dashboard_path }
+      should 'change the name' do
+        assert_equal 'New Name', assigns(:restaurant).name
+      end
     end
   end
 end
