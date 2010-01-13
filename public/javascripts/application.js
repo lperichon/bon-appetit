19ad7f42a90dbce2f89ae69c19ac548a87c0c0a8
Luis Perichon <luigi@luigi-laptop.(none)>
2010-01-13 12:55:15 -0300

    center modal

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // This sets up the proper header for rails to understand the request type,
 // and therefore properly respond to js requests (via respond_to block, for example)
+
+function open_modal(element) {
+  //Get the window height and width
+  var winH = $(window).height();
+  console.log(winH);
+  var winW = $(window).width();
+  console.log(winW);
+
+  //Set the popup window to center
+  $('#' + element + '.modal').css('top', winH/2-$('#' + element + '.modal').height()/2);
+  $('#' + element + '.modal').css('left', winW/2-$('#' + element + '.modal').width()/2);
+
+  //Show the popup window
+  $('#' + element + '.modal').show();
+}
