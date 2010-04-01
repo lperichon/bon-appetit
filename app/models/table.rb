207137f0144caff193f6793a68fb3e5fb445ad95
Luis Perichon <luigi@luigi-laptop.(none)>
2010-04-01 20:38:23 -0300

    table status

--- app/models/table.rb
+++ app/models/table.rb
@@ -5,8 +5,26 @@ class Table < ActiveRecord::Base
   validates_presence_of :restaurant, :max_party, :type
   validates_numericality_of :max_party
 
+  has_many :orders
+
   def after_initialize
     self.top ||= 0
     self.left ||= 0
+  end
+
+  def free?
+    self.open_orders.empty?
+  end
+
+  def occupied?
+    !self.free?
+  end
+
+  def open_orders
+    self.orders.opened
+  end
+
+  def status
+    self.free? ? :free : :occupied
   end
 end
